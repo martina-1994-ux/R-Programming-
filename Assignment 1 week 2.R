bce79ba21504dcf32c48e6f7bb005ce44eb9114a
martina-1994-ux <74254418+martina-1994-ux@users.noreply.github.com>
2020-11-10 17:13:48 +0100

    Add files via upload

---
+++ Assignment 1 week 2.R
@@ -0,0 +1,171 @@
+###part 1
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  
+  #path <- file.path(getwd(),directory)
+  #setwd(file.path(getwd(),directory))
+  
+  path <- paste0(getwd(),"/", directory) #this one is more general
+  
+  data <- data.frame()
+  for (i in id) {
+    if (i < 10) {
+      dat <- read.csv(paste(path,"/00", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+      data <- rbind(data,dat)
+    }
+    else if (i < 100) {
+      dat <- read.csv(paste(path,"/0", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+      data <- rbind(data,dat)
+    }
+    else {
+      dat <- read.csv(paste(path,"/", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+      data <- rbind(data,dat)
+    }
+    
+  }
+  return(mean(data[,pollutant], na.rm = T))
+}
+
+
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "sulfate", 70:72)
+pollutantmean("specdata", "sulfate", 23)
+
+
+### part 2
+
+
+complete <- function(directory, id = 1:332) {
+  path <- paste0(getwd(),"/", directory)
+  data <- data.frame()
+  for (i in id) {
+    if (i < 10) {
+      dat <- read.csv(paste(path,"/00", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+      
+    }
+    else if (i < 100) {
+      dat <- read.csv(paste(path,"/0", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+      
+    }
+    else {
+      dat <- read.csv(paste(path,"/", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+      
+    }
+    nobs <- sum(complete.cases(dat))
+    data9 <- data.frame(i, nobs)
+    data <- rbind(data,data9)
+    
+  }
+  return(data)
+}
+
+
+complete("specdata", 1)
+complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
+
+
+###part3
+
+corr <- function(directory, threshold = 0) {
+  path <- paste0(getwd(),"/", directory)
+  corr_vect <- NULL
+  for (i in 1:332) {
+    if (i < 10) {
+      dat <- read.csv(paste(path,"/00", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+    }
+    else if (i < 100) {
+      dat <- read.csv(paste(path,"/0", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+    }
+    else {
+      dat <- read.csv(paste(path,"/", as.character(i),".csv", sep = ""), 
+                      as.is = T, 
+                      header = T)
+    }
+    
+    data <- dat[complete.cases(dat),]
+    if (nrow(data) > threshold) {
+      corr_vect <- c(corr_vect, cor(data[,"sulfate"], data[, "nitrate"]))
+    }
+  }
+  
+  return(corr_vect)
+}
+
+cr <- corr("specdata", 150)
+head(cr,6)
+
+summary(cr)
+
+cr <- corr("specdata", 400)
+head(cr,6)
+
+summary(cr)
+
+cr <- corr("specdata", 5000)
+summary(cr)
+
+length(cr)
+
+cr <- corr("specdata")
+summary(cr)
+
+length(cr)
+
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "sulfate", 34)
+pollutantmean("specdata", "nitrate")
+
+cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
+print(cc$nobs)
+
+cc <- complete("specdata", 54)
+print(cc$nobs)
+
+set.seed(42)
+cc <- complete("specdata", 332:1)
+use <- sample(332, 10)
+print(cc[use, "nobs"])
+
+cr <- corr("specdata")                
+cr <- sort(cr)                
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)                
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
+
+
+
+
+
+
+
+
